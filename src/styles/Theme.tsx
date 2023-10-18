6852d08db132a693510489b4300bd1259e1e532d
Дмитрий Заичков <toropagka@gmail.com>
2023-10-18 22:49:08 +0300

    add colors to the palette

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,8 +1,25 @@
 import { createTheme } from '@mui/material';
 import { ThemeProvider } from 'styled-components';
-import { ReactNode } from 'react';
+
+const palette = {
+  primary: {
+    main: '#0D1821',
+    light: '#F0F4EF',
+  },
+  secondary: {
+    main: '#BFCC94',
+    dark: '#8D9F4F',
+  },
+  text: {
+    primary: '#B9BAC3',
+  },
+  info: {
+    main: '#344966',
+  },
+};
 
 const theme = createTheme({
+  palette,
   components: {
     // Name of the component
     MuiButton: {
@@ -17,6 +34,10 @@ const theme = createTheme({
   },
 });
 
-export const Theme = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+interface ThemePropsType {
+  children: React.ReactNode;
+}
+
+export const Theme: React.FC<ThemePropsType> = (props) => {
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 };
